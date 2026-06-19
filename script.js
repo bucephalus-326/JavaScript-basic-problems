6531d4c3155471a4d4d941055fb18e6992ab4e1f
unknown <kiranck009@gmail.com>
2026-06-19 12:15:57 +0530

    Fix logic errors

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
+
+
+
+// --------------------------
+// RE-SUBMITTING CODE 02
+// --------------------------
+
 // 1) Even or Odd
 let result;
 const check = (num) => {
-  result = num % 2;
-  if (num == 0) {
-    return (num = "Even");
-  } else {
-    return (num = "Odd");
-  }
+  return num % 2 === 0 ? "Even" : "Odd";
 };
 
 let value = check(5);
@@ -28,13 +30,12 @@ console.log("2)Result Evaluator:", result2);
 // 3) Maximum Finder — Largest of Three Numbers
 
 const findMax = (num1, num2, num3) => {
-  let biggest;
-  if (num1 > num2 && num1 > num3) {
-    return (biggest = num1);
-  } else if (num2 > num1 && num2 > num3) {
-    return (biggest = num2);
+  if (num1 >= num2 && num1 >= num3) {
+    return num1;
+  } else if (num2 >= num1 && num2 >= num3) {
+    return num2;
   } else {
-    return (biggest = num3);
+    return num3;
   }
 };
 result3 = findMax(10, 33, 44);
@@ -66,19 +67,23 @@ const printTable = function (num) {
 
 console.log("5) Multiplication");
 
-let result5 = printTable(5);
+printTable(5);
 
 // 6) Digit Counter — Number Length Finder
 const countDigits = function (num) {
+  if (num === 0) return 1;
+
   let count = 0;
+
   while (num > 0) {
     num = Math.floor(num / 10);
     count++;
   }
+
   return count;
 };
+console.log("6) Digit Counter",countDigits(3456));
 
-console.log("6) Digit Counter ", countDigits(20));
 
 // 7) Number Reverser
 
@@ -93,13 +98,17 @@ const reverseNumber = function (num) {
 
   return reversed;
 };
+console.log("7) Number reverser");
 
-console.log(reverseNumber(1234)); 
-console.log(reverseNumber(500)); 
-console.log(reverseNumber(91));  
+console.log(reverseNumber(1234));
+console.log(reverseNumber(500));
+console.log(reverseNumber(91));
 
 // 8) Factorial Engine
+
 const factorial = function (num) {
+  if (num < 0) return "Invalid input";
+
   let result = 1;
 
   for (let i = 1; i <= num; i++) {
@@ -108,10 +117,11 @@ const factorial = function (num) {
 
   return result;
 };
+console.log("8)Factorial engine");
 
 console.log(factorial(5));
-console.log(factorial(3)); 
-console.log(factorial(1)); 
+console.log(factorial(3));
+console.log(factorial(1));
 
 // 9) Prime Validator
 const isPrime = function (num) {
@@ -127,11 +137,12 @@ const isPrime = function (num) {
 
   return true;
 };
+console.log("9)Prime Validator");
 
-console.log(isPrime(7)); 
+console.log(isPrime(7));
 console.log(isPrime(10));
-console.log(isPrime(13)); 
-console.log(isPrime(1));  
+console.log(isPrime(13));
+console.log(isPrime(1));
 
 // 10) Pattern Builder — Star Triangle
 const printPattern = function (num) {
@@ -145,5 +156,6 @@ const printPattern = function (num) {
     console.log(stars);
   }
 };
+console.log("10)star pattern");
 
-printPattern(4);
+printPattern(5)
